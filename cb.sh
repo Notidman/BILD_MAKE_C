6eebfa08de535813e0ac9ab16738eda0ed389f06
Egor Zorin <81178428+Notidman@users.noreply.github.com>
2021-11-05 00:03:33 +0400

    Update cb.sh
    add -std=c18

--- cb.sh
+++ cb.sh
@@ -27,10 +27,10 @@ MAKEFILE=Makefile
 (
 cat << 'MAKETXT'
 debug : 
-	gcc -o ./bin/deb ./src/main.c -Wall -Wextra -Wpedantic
+	gcc -o ./bin/deb ./src/main.c -Wall -Wextra -Wpedantic -std=c18
 
 release :
-	gcc -o ./bin/res ./src/main.c -O3
+	gcc -o ./bin/res ./src/main.c -O3 -std=c18
 
 dl :
 	-rm -rf ./bin
