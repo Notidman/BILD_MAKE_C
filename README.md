96a43adb7dbc3c9e8d747bb71808c935f9dc0a04
notidman <laiker35@yandex.ru>
2022-07-17 08:16:33 +0400

    --replaced cmake on make, add .gitignore, fix error

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ![](img/cbc_lg.png)
 # Create Build C (cbc)
- -Bash script for building C/C++ with cmake
+ -Bash script for building C/C++ with make
 # Description: 
- -This program is designed to create a template project with cmake on c18/c++20;
- <br>-Add key '--c'   to use C in a project (default)
- <br>-Add key '--cpp' to use C++ in a project
- <br>-Add key '--git' to enable git
+ -This program is designed to create a template project with make on c18/c++20;
+ <br>-Add key '-c'   to use C in a project (default)
+ <br>-Add key '-C' to use C++ in a project
+ <br>-Add key '-g' to enable git
--- cbc.sh
+++ cbc.sh
@@ -2,7 +2,7 @@
 
 # |=======================================================================|
 # | Create Build C (cbc);
-# | Author: Ezi;
+# | Author: Notidman;
 # | Description:
 # | [[
 # |   This program is designed to create a template project
@@ -12,8 +12,8 @@
 
 # | DIR VARIABLE |========================================================|
 # |=======================================================================|
-DIR_FILES="src";
-DIR_BUILD="build";
+DIR_SRC="src";
+DIR_OBJ="obj";
 # |=======================================================================|
 
 # | COLOR VARIABLE |======================================================|
@@ -24,8 +24,6 @@ DIR_BUILD="build";
 #Brown/Orange 0;33     Yellow        1;33
 #Blue         0;34     Light Blue    1;34
 #Purple       0;35     Light Purple  1;35
-#Cyan         0;36     Light Cyan    1;36
-#Light Gray   0;37     White         1;37
 
 CR_BLACK="\033[0;30m"; 
 CR_RED="\033[0;31m"; 
@@ -109,195 +107,159 @@ check_success "main.cpp";
 
 # | Init CMakeLists.txt C++ |=============================================|
 # |=======================================================================|
-init_cmake_cpp() # $1 - name programm
+init_make_cpp() # $1 - name programm
 {
-CMAKELISTSTXT=CMakeLists.txt
+MAKELISTSTXT=Makefile
 (
-cat << CMAKETXT
-cmake_minimum_required(VERSION 3.2)
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-project($1 LANGUAGES CXX)
-set(CMAKE_CXX_STANDARD 20) 
+cat << MAKETXT
+# project name
+TARGET=$1
 
-set(CMAKE_CXX_FLAGS "\${CMAKE_CXX_FLAGS} -std=c++20 -Werror -Wall -Wextra -Wpedantic -fPIC -march=native -pthread -g")
-set(CMAKE_CXX_FLAGS_RELEASE "-std=c++20 -O2 -fPIC -march=native -pthread")
+# user way install
+PRF_USERWAY=/usr/local/bin
 
-include_directories(${DIR_FILES})
-set(PROJECT_SOURCES_DIR ${DIR_FILES})
-file(GLOB_RECURSE SOURCES \${PROJECT_SOURCES_DIR}/*.cpp)
+# project compiler
+CC=clang++
 
-add_executable(\${PROJECT_NAME} \${SOURCES})
+# cpp standart version
+DEFINES=-std=c++20
 
-CMAKETXT
-) >$CMAKELISTSTXT
+# project flags release
+CXXRFLAGS= \$(DEFINES) -O2 -fPIC -march=native
 
-check_success "CMakeLists.txt";
+# project flags debug
+CXXFLAGS= \$(DEFINES) -Wall -Werror -Wextra -Wpedantic -fPIC -march=native -g
+
+# directory with source code
+PRF_SRC=./${DIR_SRC}/
+
+# directory with object files
+PRF_OBJ=./${DIR_OBJ}/
+
+# libs
+LDLIBS=
+LIBINCLUDE=
+
+# list source files
+SRC=\$(wildcard \$(PRF_SRC)*.cpp)
+OBJ=\$(patsubst \$(PRF_SRC)%.cpp, \$(PRF_OBJ)%.o, \$(SRC))
+
+# all targets
+.PHONY : all release debug clear install uninstall docs
+
+# default make
+all : debug
+
+# target program debug
+debug : \$(OBJ)
+	\$(CC) \$(CXXFLAGS) \$(OBJ) \$(LIBINCLUDE) \$(LDLIBS) -o \$(TARGET)
+
+# target program release
+release : \$(OBJ)
+	\$(CC) \$(CXXRFLAGS) \$(OBJ) \$(LIBINCLUDE) \$(LDLIBS) -o \$(TARGET)
+
+# generate doc
+docs :
+	doxygen Doxyfile
+
+# generate .o files
+\$(PRF_OBJ)%.o : \$(PRF_SRC)%.cpp
+	\$(CC) -c \$(CXXFLAGS) $< -o \$@
+
+# clean object and binary files
+clean :
+	rm -rf \$(PRF_OBJ)*.o \$(TARGET) doc/*
+
+# install on system unix
+install :
+	install \$(TARGET) \$(PREFIX)
+
+# uninstall on system unix
+uninstall :
+	rm -rf \$(PREFIX)/\$(TARGET)
+MAKETXT
+) >$MAKELISTSTXT
+
+check_success "Makefile";
 }
 # |=======================================================================|
 
 # | Init CMakeLists.txt C |===============================================|
 # |=======================================================================|
-init_cmake_c() # $1 - name programm
+init_make_c() # $1 - name programm
 {
-CMAKELISTSTXT=CMakeLists.txt
+MAKELISTSTXT=Makefile
 (
-cat << CMAKETXT
-cmake_minimum_required(VERSION 3.2)
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-project($1 LANGUAGES C)
-set(CMAKE_C_STANDARD 18) 
+cat << MAKETXT
+# project name
+TARGET=$1
 
-set(CMAKE_C_FLAGS "\${CMAKE_C_FLAGS} -std=c18 -Werror -Wall -Wextra -Wpedantic -fPIC -march=native -pthread -g")
-set(CMAKE_C_FLAGS_RELEASE "-std=c18 -O2 -fPIC -march=native -pthread")
+# user way install
+PRF_USERWAY=/usr/local/bin
 
-include_directories(${DIR_FILES})
-set(PROJECT_SOURCES_DIR ${DIR_FILES})
-file(GLOB_RECURSE SOURCES \${PROJECT_SOURCES_DIR}/*.c)
+# project compiler
+CC=clang
 
-add_executable(\${PROJECT_NAME} \${SOURCES})
+# cpp standart version
+DEFINES=-std=c18
 
-CMAKETXT
-) >$CMAKELISTSTXT
+# project flags release
+CRFLAGS= \$(DEFINES) -O2 -fPIC -march=native
 
-check_success "CMakeLists.txt";
-}
-# |=======================================================================|
+# project flags debug
+CFLAGS= \$(DEFINES) -Wall -Werror -Wextra -Wpedantic -fPIC -march=native -g
 
-# | Init clear.sh |=======================================================|
-# |=======================================================================|
-init_clearsh() # no args
-{
-CLEARSH=clear.sh
-(
-cat << CLEARTXT
-rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake compile_commands.json Makefile
-CLEARTXT
-) >$CLEARSH
+# directory with source code
+PRF_SRC=./${DIR_SRC}/
 
-check_success "clear.sh";
-}
-# |=======================================================================|
+# directory with object files
+PRF_OBJ=./${DIR_OBJ}/
 
-# | Init run.sh |=========================================================|
-# |=======================================================================|
-init_runsh() # $1 - name programm
-{
-RUNSH=run.sh
-(
-cat << RUNTXT
-#!/bin/bash
+# libs
+LDLIBS=
+LIBINCLUDE=
 
-# | COLOR VARIABLE |======================================================|
-# |=======================================================================|
-#Black        0;30     Dark Gray     1;30
-#Red          0;31     Light Red     1;31
-#Green        0;32     Light Green   1;32
-#Brown/Orange 0;33     Yellow        1;33
-#Blue         0;34     Light Blue    1;34
-#Purple       0;35     Light Purple  1;35
-#Cyan         0;36     Light Cyan    1;36
-#Light Gray   0;37     White         1;37
+# list source files
+SRC=\$(wildcard \$(PRF_SRC)*.c)
+OBJ=\$(patsubst \$(PRF_SRC)%.c, \$(PRF_OBJ)%.o, \$(SRC))
 
-CR_BLACK="\033[0;30m"; 
-CR_RED="\033[0;31m"; 
-CR_GREEN="\033[0;32m"; 
-CR_ORANGE="\033[0;33m"; 
-CR_BLUE="\033[0;34m"; 
-CR_PURPLE="\033[0;35m"; 
-CR_CYAN="\033[0;36m"; 
-CR_LIGHT_GRAY="\033[0;37m";
+# all targets
+.PHONY : all release debug clear install uninstall docs
 
-CR_DARK_GRAY="\033[1;30m";
-CR_LIGHT_RED="\033[1;31m";
-CR_LIGHT_GREEN="\033[1;32m";
-CR_YELLOW="\033[1;33m";
-CR_LIGHT_BLUE="\033[1;34m";
-CR_LIGHT_PURPLE="\033[1;35m";
-CR_LIGHT_CYAN="\033[1;36m";
-CR_WHITE="\033[1;37m";
+# default make
+all : debug
 
-CR_END="\033[0m";
+# target program debug
+debug : \$(OBJ)
+	\$(CC) \$(CFLAGS) \$(OBJ) \$(LIBINCLUDE) \$(LDLIBS) -o \$(TARGET)
 
-# |=======================================================================|
+# target program release
+release : \$(OBJ)
+	\$(CC) \$(CRFLAGS) \$(OBJ) \$(LIBINCLUDE) \$(LDLIBS) -o \$(TARGET)
 
-# | Check success |=======================================================|
-# |=======================================================================|
-check_success() # 1 = "name part"
-{
-if [[ \$? -eq 0 ]]
-then
-  printf " -- \${CR_LIGHT_PURPLE}\$1\${CR_END} \${CR_ORANGE}was created\${CR_END} \${CR_GREEN}successfully\${CR_END}!\n"
-else
-  printf " -- \${CR_RED}ERROR:\${CR_END} \${CR_ORANGE}Script failed\${CR_END}!\n" >&2
-  exit 1
-fi
-}
-# |=======================================================================|
+# generate doc
+docs :
+	doxygen Doxyfile
 
-# | __MAIN__ |============================================================|
-# |=======================================================================|
-printf " --- [[ \${CR_ORANGE}Start run.sh\${CR_END}! ]] ---\n\n"
+# generate .o files
+\$(PRF_OBJ)%.o : \$(PRF_SRC)%.c
+	\$(CC) -c \$(CFLAGS) $< -o \$@
 
+# clean object and binary files
+clean :
+	rm -rf \$(PRF_OBJ)*.o \$(TARGET) doc/*
 
-while true; do
+# install on system unix
+install :
+	install \$(TARGET) \$(PREFIX)
 
-  printf " -- \${CR_GREEN}Debug\${CR_END} or \${CR_RED}Release\${CR_END}? (\${CR_GREEN}d\${CR_END},\${CR_RED}r\${CR_END})\n";
-  read answer;
+# uninstall on system unix
+uninstall :
+	rm -rf \$(PREFIX)/\$(TARGET)
+MAKETXT
+) >$MAKELISTSTXT
 
-  if [[ \$answer == "d"* ]]; then
-    cmake -DCMAKE_BUILD_TYPE=Debug ..
-    check_success "CMake";
-    break;
-  elif [[ \$answer == "r"* ]]; then
-    cmake -DCMAKE_BUILD_TYPE=Release ..
-    check_success "CMake";
-    break;
-  else
-    printf " --- [\${CR_RED}Incomprehensible characters\${CR_END}] '\${CR_ORANGE}\$answer\${CR_END}' ";
-  fi
-done
-
-while true; do
-
-  printf " -- \${CR_ORANGE}Do you want continue?\${CR_END} (\${CR_GREEN}y\${CR_END},\${CR_RED}N\${CR_END})\n";
-  read answer;
-
-  if [[ \$answer == "y"* ]]; then
-    make;
-    check_success "Make";
-    break;
-  elif [[ \$answer == "N"* ]]; then
-    exit 0;
-  else
-    printf " --- [\${CR_RED}Incomprehensible characters\${CR_END}] '\${CR_ORANGE}\$answer\${CR_END}' ";
-  fi
-done
-
-while true; do
-
-  printf " -- \${CR_ORANGE}Do you want continue?\${CR_END} (\${CR_GREEN}y\${CR_END},\${CR_RED}N\${CR_END})\n";
-  read answer;
-
-  if [[ \$answer == "y"* ]]; then
-    check_success "$1"
-    printf " --- [[[ \${CR_LIGHT_CYAN}$1\${CR_END} \${CR_ORANGE}was started\${CR_END}! ]]] ---\n\n"
-    ./$1;
-    printf "\n";
-    break;
-  elif [[ \$answer == "N"* ]]; then
-    exit 0;
-  else
-    printf " --- [\${CR_RED}Incomprehensible characters\${CR_END}] '\${CR_ORANGE}\$answer\${CR_END}' ";
-  fi
-done
-
-printf "\n --- [[ \${CR_ORANGE}run.sh completed\${CR_END} \${CR_LIGHT_GREEN}successfully\${CR_END}! ]] ---\n"
-exit 0;
-# |=======================================================================|
-RUNTXT
-) >$RUNSH
-
-check_success "run.sh";
+check_success "Makefile";
 }
 # |=======================================================================|
 
@@ -306,22 +268,22 @@ check_success "run.sh";
 check_ctype() # $@ args
 {
 
-if [[ $@ == *"--c" ]]; then
+if [[ $@ == *"-c"* ]]; then
   printf " -- [[[  ${CR_LIGHT_CYAN}C${CR_END} ${CR_ORANGE}project creation${CR_END}!  ]]] --\n"
-  init_cmake_c $name_dir;
-  cd ${DIR_FILES};
+  init_make_c $name_dir;
+  cd ${DIR_SRC};
   init_mainc;
   cd ..;
-elif [[ $@ == *"--cpp" ]]; then
+elif [[ $@ == *"-C"* ]]; then
   printf " -- [[[  ${CR_LIGHT_CYAN}CPP${CR_END} ${CR_ORANGE}project creation${CR_END}!  ]]] --\n"
-  init_cmake_cpp $name_dir;
-  cd ${DIR_FILES};
+  init_make_cpp $name_dir;
+  cd ${DIR_SRC};
   init_maincpp;
   cd ..;
 else
-  printf " -- [[[  ${CR_LIGHT_CYAN}C${CR_END} ${CR_ORANGE}project creation${CR_END}!  ]]] --\n"
-  init_cmake_c $name_dir;
-  cd ${DIR_FILES};
+  printf " -- [[[  ${CR_LIGHT_CYAN}[DEFAULT] C${CR_END} ${CR_ORANGE}project creation${CR_END}!  ]]] --\n"
+  init_make_c $name_dir;
+  cd ${DIR_SRC};
   init_mainc;
   cd ..;
 fi
@@ -332,11 +294,175 @@ fi
 # |=======================================================================|
 init_git() # $@ args
 {
-if [[ $@ == *"--git"* ]]; then
+if [[ $@ == *"-g"* ]]; then
   git init &> .git_log.txt;
   check_success "git";
 fi
 
+}
+# |=======================================================================|
+
+# | Init .gitignore |=====================================================|
+# |=======================================================================|
+init_gitignore() # $@ args
+{
+if [[ $@ == *"-C"* ]]; then
+CPPIGNORE=.gitignore  
+(
+cat << IGNORETXT
+# Prerequisites
+*.d
+
+# Compiled Object files
+*.slo
+*.lo
+*.o
+*.obj
+
+# Precompiled Headers
+*.gch
+*.pch
+
+# Compiled Dynamic libraries
+*.so
+*.dylib
+*.dll
+
+# Fortran module files
+*.mod
+*.smod
+
+# Compiled Static libraries
+*.lai
+*.la
+*.a
+*.lib
+
+# Executables
+*.exe
+*.out
+*.app
+IGNORETXT
+) >$CPPIGNORE
+elif [[ $@ == *"-c"* ]]; then
+CIGNORE=.gitignore  
+(
+cat << IGNORETXT
+# Prerequisites
+*.d
+
+# Object files
+*.o
+*.ko
+*.obj
+*.elf
+
+# Linker output
+*.ilk
+*.map
+*.exp
+
+# Precompiled Headers
+*.gch
+*.pch
+
+# Libraries
+*.lib
+*.a
+*.la
+*.lo
+
+# Shared objects (inc. Windows DLLs)
+*.dll
+*.so
+*.so.*
+*.dylib
+
+# Executables
+*.exe
+*.out
+*.app
+*.i*86
+*.x86_64
+*.hex
+
+# Debug files
+*.dSYM/
+*.su
+*.idb
+*.pdb
+
+# Kernel Module Compile Results
+*.mod*
+*.cmd
+.tmp_versions/
+modules.order
+Module.symvers
+Mkfile.old
+dkms.conf
+IGNORETXT
+) >$CIGNORE
+else
+CIGNORE=.gitignore  
+(
+cat << IGNORETXT
+# Prerequisites
+*.d
+
+# Object files
+*.o
+*.ko
+*.obj
+*.elf
+
+# Linker output
+*.ilk
+*.map
+*.exp
+
+# Precompiled Headers
+*.gch
+*.pch
+
+# Libraries
+*.lib
+*.a
+*.la
+*.lo
+
+# Shared objects (inc. Windows DLLs)
+*.dll
+*.so
+*.so.*
+*.dylib
+
+# Executables
+*.exe
+*.out
+*.app
+*.i*86
+*.x86_64
+*.hex
+
+# Debug files
+*.dSYM/
+*.su
+*.idb
+*.pdb
+
+# Kernel Module Compile Results
+*.mod*
+*.cmd
+.tmp_versions/
+modules.order
+Module.symvers
+Mkfile.old
+dkms.conf
+IGNORETXT
+) >$CIGNORE
+fi
+
+check_success ".gitignore";
 }
 # |=======================================================================|
 
@@ -386,16 +512,18 @@ for name_dir in $@; do
     continue;
   fi
 
+  # Make project dir
   mkdir "$name_dir";
   cd $name_dir;
-  mkdir ${DIR_BUILD} ${DIR_FILES};
-  check_ctype $@
+  # Make src and obj dirs
+  mkdir ${DIR_SRC} ${DIR_OBJ};
+  # Generate C or Cpp files
+  check_ctype $@;
+  # Init git
   init_git $@;
-  cd build;
-  init_runsh $name_dir;
-  init_clearsh;
-  chmod +x *;
-  cd ../..;
+  # Make .gitignore
+  init_gitignore $@;
+  cd ..;
 
 done
 
